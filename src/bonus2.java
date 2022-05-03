a0deb1bb7e955ed973ef4c2571c7aaf75ab47991
LokiTheDark666 <khcr.loki@mail.ru>
2022-05-03 13:16:29 +0300

    fixed ///v1.2

--- src/bonus2.java
+++
@@ -1,9 +0,0 @@
-public class bonus2 {
-    public static void main(String[] args) {
-
-        int x = 400;// цена билета
-        int y = (int) x / 20;//бонусных миль
-
-        System.out.println((y) + (" бонусных миль"));
-    }
-}
---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        int price = 400;// цена билета
+        int bonus = (int) price / 20;//бонусных миль
+
+        System.out.println((bonus) + (" бонусных миль"));
+    }
+}
